2921d73f07c20ae93daeafaf85531dc79c6c1906
buiquoclong <buiquoclongkt123@gmail.com>
2025-10-14 13:55:15 +0700

    Create utils formatDate

--- src/Utils/bookingUtils.js
+++ src/Utils/bookingUtils.js
@@ -3,3 +3,11 @@ export const SEAT_STATUS = {
   RESERVED: 2,
   AVAILABLE: 0,
 };
+
+// Format ngày
+export const formatDate = (dateString) => {
+  const date = new Date(dateString);
+  return `${String(date.getDate()).padStart(2, "0")}/${String(
+    date.getMonth() + 1
+  ).padStart(2, "0")}/${date.getFullYear()}`;
+};
