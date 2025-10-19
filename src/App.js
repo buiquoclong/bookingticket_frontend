52ddcae68454aa4be142ca917b895dfe17d03517
buiquoclong <buiquoclongkt123@gmail.com>
2025-10-19 09:19:20 +0700

    Update Router of project

--- src/App.js
+++ src/App.js
@@ -2,52 +2,52 @@ import React, { useState, useEffect } from "react";
 import "./app.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import AdminHomePage from "./page/Admin/AdminHomePage";
-// import AdminBookingDetailPage from "./Admin/page/AdminBookingDetailPage";
-import AdminBookingPage from "./page/Admin/AdminBookingPage";
-import AdminBookTicketPage from "./page/Admin/AdminBookTicketPage";
-import AdminCityPage from "./page/Admin/AdminCityPage";
-import AdminPayPage from "./page/Admin/AdminPayPage";
-import AdminVehiclePage from "./page/Admin/AdminVehiclePage";
-import AdminSeatPage from "./page/Admin/AdminSeatPage";
-import AdminRoutePage from "./page/Admin/AdminRoutePage";
-import AdminTripPage from "./page/Admin/AdminTripPage";
-import AdminSeatReservationPage from "./page/Admin/AdminSeatReservationPage";
-import AdminUserPage from "./page/Admin/AdminUserPage";
-import AdminBookTicketReturnPage from "./page/Admin/AdminBookTicketReturnPage";
-import AdminDriverPage from "./page/Admin/AdminDriverPage";
-import AdminContactPage from "./page/Admin/AdminContactPage";
-import AdminLogPage from "./page/Admin/AdminLogPage";
-import AdminPromotionPage from "./page/Admin/AdminPromotionPage";
-import AdminReviewPage from "./page/Admin/AdminReviewPage";
-import AdminPaySuccessPage from "./page/Admin/AdminPaySuccessPage";
 import NotFoundPage from "./page/Admin/NotFoundPage";
-import AdminForBookingPage from "./page/Admin/AdminForBookingPage";
-import AdminCatchPointPage from "./page/Admin/AdminCatchPointPage";
-import AdminKindvehiclePage from "./page/Admin/AdminKindvehiclePage";
 import CustomToastContainer from "./Components/ComponentParts/CustomToastContainer";
 import Layout from "./page/Client/Layout";
-import Home from "./Components/ComponentPages/ClientPages/Home/Home";
-import SearchRoute from "./Components/ComponentPages/ClientPages/SearchRoute/SearchRoute";
-import SearchTicket from "./Components/ComponentPages/ClientPages/SearchTicket/SearchTicket";
-import Login from "./Components/ComponentPages/ClientPages/Login/Login";
-import Register from "./Components/ComponentPages/ClientPages/Register/Register";
-import BookingTicket from "./Components/ComponentPages/ClientPages/BookingTicket/BookingTicket";
+import Home from "./Components/ComponentPages/ClientPages/Home";
+import SearchRoute from "./Components/ComponentPages/ClientPages/SearchRoute";
+import SearchTicket from "./Components/ComponentPages/ClientPages/SearchTicket";
+import Login from "./Components/ComponentPages/ClientPages/Login";
+import Register from "./Components/ComponentPages/ClientPages/Register";
+import BookingTicket from "./Components/ComponentPages/ClientPages/BookingTicket";
 import BookTicket from "./Components/ComponentPages/ClientPages/BookTicket/BookTicket";
 import BookTicketReturn from "./Components/ComponentPages/ClientPages/BookTicket/BookTicketReturn";
-import InfoUser from "./Components/ComponentPages/ClientPages/InfoUser/InfoUser";
-import Paysuccess from "./Components/ComponentPages/ClientPages/Paysuccess/Paysuccess";
-import Contact from "./Components/ComponentPages/ClientPages/Contact/Contact";
-import AboutUs from "./Components/ComponentPages/ClientPages/AboutUs/AboutUs";
-import MyRating from "./Components/ComponentPages/ClientPages/MyRating/MyRating";
-import MyBooking from "./Components/ComponentPages/ClientPages/MyBooking/MyBooking";
-import ChangePass from "./Components/ComponentPages/ClientPages/ChangePass/ChangePass";
-import ConfirmAccount from "./Components/ComponentPages/ClientPages/ConfirmAccount/ConfirmAccount";
-import ForgetPass from "./Components/ComponentPages/ClientPages/ForgetPass/ForgetPass";
+import InfoUser from "./Components/ComponentPages/ClientPages/InfoUser";
+import Paysuccess from "./Components/ComponentPages/ClientPages/Paysuccess";
+import Contact from "./Components/ComponentPages/ClientPages/Contact";
+import AboutUs from "./Components/ComponentPages/ClientPages/AboutUs";
+import MyRating from "./Components/ComponentPages/ClientPages/MyRating";
+import MyBooking from "./Components/ComponentPages/ClientPages/MyBooking";
+import ChangePass from "./Components/ComponentPages/ClientPages/ChangePass";
+import ConfirmAccount from "./Components/ComponentPages/ClientPages/ConfirmAccount";
+import ForgetPass from "./Components/ComponentPages/ClientPages/ForgetPass";
 import ResponseSuccess from "./Components/ComponentPages/ClientPages/ResponsePay/ResponseSuccess";
 import ResponseFailed from "./Components/ComponentPages/ClientPages/ResponsePay/ResponseFailed";
-import TicketHistory from "./Components/ComponentPages/ClientPages/TicketHistory/TicketHistory";
+import TicketHistory from "./Components/ComponentPages/ClientPages/TicketHistory";
 import ProtectedRoute from "./Components/ComponentParts/ProtectedRoute";
+import AdminLayout from "./page/Admin/AdminLayout";
+import AdminHome from "./Components/ComponentPages/AdminPages/AdminHome/AdminHome";
+import AdminBooking from "./Components/ComponentPages/AdminPages/AdminBooking";
+import AdminForBookTicket from "./Components/ComponentPages/AdminPages/AdminForBookTicket/AdminForBookTicket";
+import AdminBookTicket from "./Components/ComponentPages/AdminPages/AdminBookTicket/AdminBookTicket";
+import AdminBookTicketReturn from "./Components/ComponentPages/AdminPages/AdminBookTicket/AdminBookTicketReturn";
+import AdminPaySuccess from "./Components/ComponentPages/AdminPages/AdminPaySuccess/AdminPaySuccess";
+import AdminCity from "./Components/ComponentPages/AdminPages/AdminCity";
+import AdminPay from "./Components/ComponentPages/AdminPages/AdminPay/AdminPay";
+import AdminRoute from "./Components/ComponentPages/AdminPages/AdminRoute";
+import AdminVehicle from "./Components/ComponentPages/AdminPages/AdminVehicle";
+import AdminSeat from "./Components/ComponentPages/AdminPages/AdminSeat";
+import AdminTrip from "./Components/ComponentPages/AdminPages/AdminTrip";
+import AdminSeatReservation from "./Components/ComponentPages/AdminPages/AdminSeatReservation";
+import AdminUser from "./Components/ComponentPages/AdminPages/AdminUser";
+import AdminDriver from "./Components/ComponentPages/AdminPages/AdminDriver";
+import AdminContact from "./Components/ComponentPages/AdminPages/AdminContact";
+import AdminLog from "./Components/ComponentPages/AdminPages/AdminLog";
+import AdminPromotion from "./Components/ComponentPages/AdminPages/AdminPromotion";
+import AdminReview from "./Components/ComponentPages/AdminPages/AdminReview";
+import AdminCatchPoint from "./Components/ComponentPages/AdminPages/AdminCatchPoint";
+import AdminKindVehicle from "./Components/ComponentPages/AdminPages/AdminKindVehicle";
 
 const App = () => {
   const [userRole, setUserRole] = useState(null);
@@ -95,69 +95,67 @@ const App = () => {
         </Route>
 
         <Route element={<ProtectedRoute allowedRoles={[2, 3]} />}>
-          <Route path="/admin" element={<AdminHomePage />} />
-          <Route path="/admin/bookings" element={<AdminBookingPage />} />
-          <Route path="/admin/book-ticket" element={<AdminForBookingPage />} />
-          <Route path="/admin/find-trips" element={<AdminBookTicketPage />} />
-          <Route
-            path="/admin/find-trips-return"
-            element={<AdminBookTicketReturnPage />}
-          />
-          <Route
-            path="/admin/book-cash-payment"
-            element={<AdminPaySuccessPage />}
-          />
-          <Route
-            path="/admin/cities"
-            element={userRole === 3 ? <AdminCityPage /> : <NotFoundPage />}
-          />
-          <Route path="/admin/booking-trip" element={<AdminPayPage />} />
-          <Route
-            path="/admin/routes"
-            element={userRole === 3 ? <AdminRoutePage /> : <NotFoundPage />}
-          />
-          <Route
-            path="/admin/vehicles"
-            element={userRole === 3 ? <AdminVehiclePage /> : <NotFoundPage />}
-          />
-          <Route
-            path="/admin/seats"
-            element={userRole === 3 ? <AdminSeatPage /> : <NotFoundPage />}
-          />
-          <Route path="/admin/trips" element={<AdminTripPage />} />
-          <Route
-            path="/admin/seat-reservation"
-            element={<AdminSeatReservationPage />}
-          />
-          <Route
-            path="/admin/users"
-            element={userRole === 3 ? <AdminUserPage /> : <NotFoundPage />}
-          />
-          <Route
-            path="/admin/drivers"
-            element={userRole === 3 ? <AdminDriverPage /> : <NotFoundPage />}
-          />
-          <Route path="/admin/contacts" element={<AdminContactPage />} />
-          <Route
-            path="/admin/logs"
-            element={userRole === 3 ? <AdminLogPage /> : <NotFoundPage />}
-          />
-          <Route path="/admin/promotions" element={<AdminPromotionPage />} />
-          <Route path="/admin/reviews" element={<AdminReviewPage />} />
-          <Route
-            path="/admin/catch-point"
-            element={
-              userRole === 3 ? <AdminCatchPointPage /> : <NotFoundPage />
-            }
-          />
-          <Route
-            path="/admin/kind-vehicle"
-            element={
-              userRole === 3 ? <AdminKindvehiclePage /> : <NotFoundPage />
-            }
-          />
+          <Route element={<AdminLayout />}>
+            <Route path="/admin" element={<AdminHome />} />
+            <Route path="/admin/bookings" element={<AdminBooking />} />
+            <Route path="/admin/book-ticket" element={<AdminForBookTicket />} />
+            <Route path="/admin/find-trips" element={<AdminBookTicket />} />
+            <Route
+              path="/admin/find-trips-return"
+              element={<AdminBookTicketReturn />}
+            />
+            <Route
+              path="/admin/book-cash-payment"
+              element={<AdminPaySuccess />}
+            />
+            <Route
+              path="/admin/cities"
+              element={userRole === 3 ? <AdminCity /> : <NotFoundPage />}
+            />
+            <Route path="/admin/booking-trip" element={<AdminPay />} />
+            <Route
+              path="/admin/routes"
+              element={userRole === 3 ? <AdminRoute /> : <NotFoundPage />}
+            />
+            <Route
+              path="/admin/vehicles"
+              element={userRole === 3 ? <AdminVehicle /> : <NotFoundPage />}
+            />
+            <Route
+              path="/admin/seats"
+              element={userRole === 3 ? <AdminSeat /> : <NotFoundPage />}
+            />
+            <Route path="/admin/trips" element={<AdminTrip />} />
+            <Route
+              path="/admin/seat-reservation"
+              element={<AdminSeatReservation />}
+            />
+            <Route
+              path="/admin/users"
+              element={userRole === 3 ? <AdminUser /> : <NotFoundPage />}
+            />
+            <Route
+              path="/admin/drivers"
+              element={userRole === 3 ? <AdminDriver /> : <NotFoundPage />}
+            />
+            <Route path="/admin/contacts" element={<AdminContact />} />
+            <Route
+              path="/admin/logs"
+              element={userRole === 3 ? <AdminLog /> : <NotFoundPage />}
+            />
+            <Route path="/admin/promotions" element={<AdminPromotion />} />
+            <Route path="/admin/reviews" element={<AdminReview />} />
+            <Route
+              path="/admin/catch-point"
+              element={userRole === 3 ? <AdminCatchPoint /> : <NotFoundPage />}
+            />
+            <Route
+              path="/admin/kind-vehicle"
+              element={userRole === 3 ? <AdminKindVehicle /> : <NotFoundPage />}
+            />
+          </Route>
+          <Route path="*" element={<NotFoundPage />} />
         </Route>
-        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   );
