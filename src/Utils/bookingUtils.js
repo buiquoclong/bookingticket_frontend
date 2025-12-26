e9eb7a5c1380923f866d4f1b0ae92f67f44866b5
buiquoclong <buiquoclongkt123@gmail.com>
2025-12-26 21:20:37 +0700

    Update logic and comment in booking Utils

--- src/Utils/bookingUtils.js
+++ src/Utils/bookingUtils.js
@@ -6,7 +6,7 @@ export const SEAT_STATUS = {
 export function getCurrentDateTimeLocal() {
   const now = new Date();
   const year = now.getFullYear();
-  const month = (now.getMonth() + 1).toString().padStart(2, "0"); // thêm '0' nếu cần
+  const month = (now.getMonth() + 1).toString().padStart(2, "0");
   const day = now.getDate().toString().padStart(2, "0");
   return `${year}-${month}-${day}`;
 }
